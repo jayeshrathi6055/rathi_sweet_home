db38fe3131bd96656abfabb98192bcec615d99d6
Rathi, Jayesh <jayeshrathi6055@gmail.com>
2025-08-10 00:50:36 +0530

    Added view expense feature

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 rathi_sweet_home_database = RathiSweetHomeDatabase(app)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/employeeManagement')
-def employee_management():
-    return render_template("employee_management/employee_management_dashboard.html")
 
 @app.route('/employeeManagement/allEmployees')
 def all_employees():
-    return render_template("employee_management/all_employees.html", employees = rathi_sweet_home_database.fetch_employee())
+    return render_template("employee_management/all_employees.html",
+                           employees=rathi_sweet_home_database.fetch_employee())
+
 
 @app.route('/employeeManagement/allEmployees/actionEmployee', methods=['POST'])
 def action_employee():
@@ -45,7 +45,9 @@ def view_transactions():
     filters = request.args.to_dict()
     user_transactions = rathi_sweet_home_database.fetch_user_transactions(**filters)
     users = rathi_sweet_home_database.fetch_employee()
-    return render_template("employee_management/view_transactions.html", user_transactions = user_transactions, users = users)
+    return render_template("employee_management/view_transactions.html", user_transactions=user_transactions,
+                           users=users)
+
 
 @app.route('/employeeManagement/saveTransactions', methods=['GET', 'POST'])
 def save_transactions():
@@ -53,13 +55,21 @@ def save_transactions():
         data = request.form.to_dict()
         employee_transaction = EmployeeTransaction(**data)
         rathi_sweet_home_database.save_transaction(employee_transaction)
-    return render_template("employee_management/save_transactions.html", employees = rathi_sweet_home_database.fetch_employee())
+    return render_template("employee_management/save_transactions.html",
+                           employees=rathi_sweet_home_database.fetch_employee())
+
 
 @app.route('/expenses')
 def expenses():
+    filters = request.args.to_dict()
+    date_of_expenses = filters.get('date', datetime.now(ZoneInfo("Asia/Kolkata")).date().isoformat())
+    expenses_by_date = rathi_sweet_home_database.fetch_expenses(date_of_expenses)
+    total_of_expenses = sum([expense['amount'] for expense in expenses_by_date])
     expense_categories = rathi_sweet_home_database.fetch_expense_categories()
-    expenses_by_date = rathi_sweet_home_database.fetch_expenses(datetime.now(ZoneInfo("Asia/Kolkata")).date().isoformat())
-    return render_template('expenses.html', expense_categories = expense_categories, expenses = expenses_by_date)
+    return render_template('expenses.html',
+                           expense_categories=expense_categories,
+                           expenses=expenses_by_date, total_of_expenses=total_of_expenses)
+
 
 @app.route('/expenses/addExpense', methods=['POST'])
 def add_expense():
@@ -68,6 +78,7 @@ def add_expense():
     rathi_sweet_home_database.save_expense(expense)
     return redirect('/expenses')
 
+
 @app.route('/expenses/addExpenseCategory', methods=['POST'])
 def add_expense_category():
     data = request.form.to_dict()
@@ -75,6 +86,6 @@ def add_expense_category():
     rathi_sweet_home_database.save_expense_category(expense_category)
     return redirect('/expenses')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
