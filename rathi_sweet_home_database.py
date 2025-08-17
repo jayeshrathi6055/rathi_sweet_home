c2d5c8dd7f612015288699509c05ee486c9a0942
Rathi, Jayesh <jayeshrathi6055@gmail.com>
2025-08-17 19:37:50 +0530

    Fixed sorting issue

--- rathi_sweet_home_database.py
+++ rathi_sweet_home_database.py
@@ -15,9 +15,8 @@ class RathiSweetHomeDatabase:
     def fetch_employee(self, filter_args=None):
         if filter_args is None:
             filter_args = {"type": UserType.EMPLOYEE}
-        employees = self.__user_collection.find(filter_args)
-        sorted_employees = sorted(employees, key=lambda employee: employee.get('created_at', datetime.now()), reverse=True)
-        return tuple({**emp, "_id": str(emp["_id"])} for emp in sorted_employees)
+        employees = self.__user_collection.find(filter_args).sort("created_at", -1)
+        return tuple({**emp, "_id": str(emp["_id"])} for emp in employees)
 
     def save_employee(self, employee:Employee):
         return self.__user_collection.insert_one(EmployeeMapper.for_save_dict(employee))
@@ -32,19 +31,24 @@ class RathiSweetHomeDatabase:
 
     def fetch_user_transactions(self, **kwargs):
         filter_args = {}
-        limit = int(kwargs.get('transactions_limit', 0))
+        limit = int(kwargs.get('transactions_limit', 7))
         user_id = kwargs.get('user_id')
-        if not limit:
-            limit = 7
         if user_id:
             filter_args["user_id"] = ObjectId(user_id)
-        all_transactions = tuple(self.__transaction_collection.find(filter_args).limit(limit))
-        sorted_transactions = sorted(all_transactions, key=lambda t: datetime.fromisoformat(t.get("created_at", datetime.now())), reverse=True)
+
+        all_transactions = tuple(
+            self.__transaction_collection
+            .find(filter_args)
+            .sort("created_at", -1)
+            .limit(limit)
+        )
+
         user_transactions = []
-        for i in sorted_transactions:
+        for i in all_transactions:
             i['user_name'] = self.__user_collection.find_one({"_id": ObjectId(i['user_id'])})['name']
             user_transactions.append(i)
-        return user_transactions
+
+        return all_transactions
 
     def save_transaction(self, employee_transaction: EmployeeTransaction):
         # Convert transaction to dictionary format
